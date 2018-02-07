984d4619b683d7031d30e68b600d55f6fd8516ef
Edwin Chi Pineiro <chipineiro@gmail.com>
2018-02-06 22:37:00 -0600

    Más ejemplos de funciones

--- 3. Funciones.php
+++ 3. Funciones.php
@@ -15,5 +15,17 @@
 
     <p>El cubo de 2 es <?php echo cubo(2); ?></p>
     <p>El cubo de 5 es <?php echo cubo(5); ?></p>
+    <hr>
+
+    <h1>Ejemplo 1</h1>
+
+    <?php
+    function salario($salarioMin = 100){
+      echo "El salario es de $salarioMin";
+    }
+    salario();  //Outputs El salario es de 100
+    ?>
+    <p><?php salario(300); ?></p> <!--Outputs El salario es de 300-->
+    <p><?php salario(5000); ?></p> <!--Outputs El salario es de 5000-->
   </body>
 </html>
